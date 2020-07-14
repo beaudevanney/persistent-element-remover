880270de0b0ab464037e5e7f36ab1492efb7c399
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-07-14 21:42:46 +0200

    refactor

--- content-script.js
+++ content-script.js
@@ -1,33 +1,33 @@
 
-async function remove_elements() {
-	let res = await browser.storage.local.get('selectors');
-	if ( !Array.isArray(res.selectors) ) { return; }
-	res.selectors.forEach( (selector) => {
-		if(typeof selector.url_regex !== 'string') { return; }
-		if (selector.url_regex.trim() === ''){ return; }
-		let re = new RegExp(selector.url_regex, 'g');
-		let current_url = window.location.href;
-		if(re.test(current_url)){
-			//console.log('current url matches regex', selector.url_regex, current_url);
-			if ( typeof selector.selectors !== 'string' ) { return; }
-			selector.selectors.split("\n").forEach( (sel) => {
-				if (typeof sel !== 'string') { return; }
-				if(sel.trim() === '') { return;	}
-				document.querySelectorAll(sel).forEach ( (el) => {
-					if(typeof el.remove === 'function'){
-						//console.log('removed element', sel);
-						el.remove();
-					}
-				});
+const current_url = window.location.href;
 
-			});
-		}
+function remove() {
+	browser.storage.local.get('selectors').then( (res) => { 
+		if ( !Array.isArray(res.selectors) ) { return; }
+		res.selectors.forEach( (selector) => {
+			if(typeof selector.url_regex !== 'string') { return; }
+			if (selector.url_regex.trim() === ''){ return; }
+			let re = new RegExp(selector.url_regex, 'g');
+			if(re.test(current_url)){
+				if ( typeof selector.selectors !== 'string' ) { return; }
+				selector.selectors.split("\n").forEach( (sel) => {
+					if (typeof sel !== 'string') { return; }
+					if(sel.trim() === '') { return;	}
+					document.querySelectorAll(sel).forEach ( (el) => {
+						if(typeof el.remove === 'function'){
+							el.remove();
+							console.log('removed element', current_url , sel);
+						}
+					});
+
+				});
+			}
+		});
 	});
 }
 
 // run on first page load
-remove_elements();
+remove();
 
 // run each time the document changes (e.g. when posts are dynamically added on scroll down)
-var observer = new MutationObserver(remove_elements);
-observer.observe(document, { attributes: false, childList: true, subtree: true }); 
+(new MutationObserver(remove)).observe(document.body, { attributes: false, childList: true, subtree: true }); 
