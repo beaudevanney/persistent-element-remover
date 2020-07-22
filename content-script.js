35e30bacc2d79b108e4f1ed999d120b2d24beb49
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-07-22 16:26:17 +0200

    feat: improvment performance registering observer only when abs. required

--- content-script.js
+++ content-script.js
@@ -1,51 +1,57 @@
 
-// save url on page load
-const current_url = window.location.href;
+(async function() {
 
-async function remove() {
+	const generators = [];
+	// save url on page load
+	const current_url = window.location.href;
 
-	try { 
-		const res = await browser.storage.local.get('selectors');
-
-		if ( !Array.isArray(res.selectors) ) { return; }
-
-		res.selectors.forEach( (selector) => {
-
-			// check activ state of rule
-			if(typeof selector.activ !== 'boolean') { return; }
-			if(selector.activ !== true) { return; }
-
-			// check url regex 
-			if(typeof selector.url_regex !== 'string') { return; }
-			if(selector.url_regex === ''){ return; }
-
-			let re = new RegExp(selector.url_regex, 'g');
-			if(re.test(current_url)){
-				if ( typeof selector.code !== 'string' ) { return; }
-				if ( selector.code === '' ) { return; }
-
-				try {
-					//els = window.eval('(function(){ ' + selector.code + ' })()');
-					let generator = new Function(selector.code);
-					generator().forEach( (el) => {
-						if(typeof el.remove === 'function'){
-							el.remove();
-							console.log('removed element', current_url);
-						}
-					});
-				}catch(e){
-					console.error(e, selector.code);
+	async function remove() {
+		generators.forEach( (generator) => {
+			generator().forEach( (el) => {
+				if(typeof el.remove === 'function'){
+					el.remove();
+					console.log('removed element');
 				}
-			}
+			});
 		});
-	}catch(e) {
-		console.error(e);
 	}
-}
 
-// run on first page load
-remove();
+	// get rules from storage
+	const res = await browser.storage.local.get('selectors');
+	if ( !Array.isArray(res.selectors) ) { return; }
 
-// run each time the document changes (e.g. when posts are dynamically added on scroll down)
-(new MutationObserver(remove)).observe(document.body, { attributes: false, childList: true, subtree: true }); 
+	res.selectors.forEach( (selector) => {
 
+		// check activ state of rule
+		if(typeof selector.activ !== 'boolean') { return; } 
+		if(selector.activ !== true) { return; }
+
+		// check url regex 
+		if(typeof selector.url_regex !== 'string') { return; }
+		if(selector.url_regex === ''){ return; }
+
+		const re = new RegExp(selector.url_regex, 'g');
+
+		if(re.test(current_url)){
+
+			if ( typeof selector.code !== 'string' ) { return; }
+			if ( selector.code === '' ) { return; }
+
+			try {
+				gen = new Function(selector.code)
+			
+				// save generator to rerun when something changes
+				generators.push(gen)
+			}catch(e){
+				console.error('generator code is invalid, skipping: ', selector.code);
+			}
+
+		}
+	});
+
+	if( generators.length > 0) {
+		console.log('registered persistend-element-remover');
+		remove();
+		(new MutationObserver(remove)).observe(document.body, { attributes: false, childList: true, subtree: true }); 
+	}
+})();
