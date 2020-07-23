5243f90c0f427c563d310397a605af4669077f46
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-07-23 15:47:57 +0200

    refactor: cleanup

--- content-script.js
+++ content-script.js
@@ -1,15 +1,18 @@
 
 (async () => {
 
+	let store;
 	const extId = 'PER';
 	const generators = [];
-	const current_url = window.location.href;
+	const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
 
 	const log = (level, msg) => { 
-		if (['debug', 'info','log','error','warn'].includes(level)){
-			console[level](extId + '::' + level + '::' + msg); 
-		}else{
-			log('error', extId + 'invalued level argument "' + level + '" for function log(level,msg)');
+		level = level.trim().toLowerCase();
+		if (['error','warn'].includes(level) 
+			|| ( temporary && ['debug','info','log'].includes(level))
+		) {
+			console[level](extId + '::' + level.toUpperCase() + '::' + msg); 
+			return;
 		}
 	}
 
@@ -18,65 +21,63 @@
 		els.forEach( (el) => { 
 			if(typeof el.remove === 'function'){
 				el.remove();
-				log('debug','element removed');
-			}else{
-				log('debug','element not removed (missing remove() function)');
+				log('DEBUG','element removed');
 			}
 		});
 	}
 
 	const onChange = () => {
-		log('debug','onChange()'+ generators.length);
-		if ( typeof generators.forEach !== 'function') { return; }
 		generators.forEach( (gen) => { remove_elements(gen()); });
 	}
 
 
 	try {
-		const store = await browser.storage.local.get('selectors');
+		store = await browser.storage.local.get('selectors');
+	}catch(e){
+		log('ERROR', 'access to rules storage failed');
+		return;
+	}
 
-		if ( typeof store.selectors.forEach !== 'function' ) { throw 'selectors has no forEach'; }
+	if ( typeof store.selectors.forEach !== 'function' ) { 
+		log('ERROR', 'rules selectors not iterable');
+		return;
+	}
 
-		store.selectors.forEach( (selector) => {
+	store.selectors.forEach( (selector) => {
 
-			// check activ
-			if(typeof selector.activ !== 'boolean') { return; }
-			if(selector.activ !== true) { return; }
+		// check activ
+		if(typeof selector.activ !== 'boolean') { return; }
+		if(selector.activ !== true) { return; }
 
-			// check url regex 
-			if(typeof selector.url_regex !== 'string') { return; }
-			if(selector.url_regex === ''){ return; }
+		// check url regex 
+		if(typeof selector.url_regex !== 'string') { return; }
+		selector.url_regex = selector.url_regex.trim(); 
+		if(selector.url_regex === ''){ return; }
 
-			try { 
-				const re = new RegExp(selector.url_regex, 'g');
-				if(!re.test(current_url)){ return; }
-			} catch(e) {
-				log('warn', 'invalid url regex : ' + selectors.url_regex);
-				return;
-			}
-
-			if ( typeof selector.code !== 'string' ) { return; }
-			if ( selector.code === '' ) { return; }
-
-			try {
-				const gen = new Function(selector.code); // build function
-				remove_elements(gen()); // execute function
-				generators.push(gen); // store function 
-			}catch(e){
-				log('warn', 'code execution failed :' + selectors.code);
-			}
-		});
-
-		if(generators.length > 0){
-			log('info', 'registered mutation observer');
-			(new MutationObserver(onChange)).observe(document.body, { attributes: false, childList: true, subtree: true }); 
-		}else{
-			log('debug','no matching rules');
+		try { 
+			if(!(new RegExp(selector.url_regex)).test(window.location.href)){ return; }
+		} catch(e) {
+			log('WARN', 'invalid url regex : ' + selectors.url_regex);
+			return;
 		}
 
-	}catch(e){
-		log('error', 'access to local storage failed');
-		return;
+		if ( typeof selector.code !== 'string' ) { return; }
+		if ( selector.code === '' ) { return; }
+
+		try {
+			const gen = new Function(selector.code); // build function
+			remove_elements(gen()); // execute function
+			generators.push(gen); // store function 
+		}catch(e){
+			log('WARN', 'code execution failed :' + selectors.code);
+		}
+	});
+
+	if(generators.length > 0){
+		log('INFO', 'registered mutation observer');
+		(new MutationObserver(onChange)).observe(document.body, { attributes: false, childList: true, subtree: true }); 
+	}else{
+		log('DEBUG','no matching rules');
 	}
 
 })();
