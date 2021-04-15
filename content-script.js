cc0ff0b17f55d96ea4d46ea4cd6d225690c205c4
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-04-15 14:45:21 +0200

    fix: code starts with return statement

--- content-script.js
+++ content-script.js
@@ -69,7 +69,7 @@
 			// if the "code" contains a 'return ' string, it is most likely js code
 			// this is not perfect, but ... changing the storage layout ... to include another option would break some peopls config, which ... is kind of awful
 			// TODO: rewrite options.js to update old storage layout and include switch instead of this "hack"
-			if(/[\n \(;]return[\n \(;]/.test(selector.code)) {
+			if(/^|[\n \(;]return[\n \(;]/.test(selector.code)) {
 				gen = new Function(selector.code); // build function
 			} else { // otherwise assume it is a selector 
 				gen = function() { return document.querySelectorAll(selector.code); }
